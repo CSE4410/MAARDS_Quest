20c3588630400a0dc80f17a575aa0c8287464b19
rubenpuentes2000 <100728947+rubenpuentes2000@users.noreply.github.com>
2023-03-30 13:34:23 -0700

    AnimationStrings script

---
+++ MAARD/Assets/Scripts/AnimationStrings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class AnimationStrings
+{
+    internal static string isMoving = "isMoving";
+    internal static string isRunning = "isRunning";
+}
