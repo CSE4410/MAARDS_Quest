665c057c879f493197cb55ce0427566809ed1483
Ruben Puentes <rubenpuentes2000@gmail.com>
2023-04-17 20:12:52 -0700

    knight improvments

--- MAARD/Assets/Scripts/Knight.cs
+++ MAARD/Assets/Scripts/Knight.cs
@@ -9,6 +9,7 @@ public class Knight : MonoBehaviour
     public float walkSpeed = 3f;
     public float walkStopRate = 0.05f;
     public DetectionZone attackZone;
+    public DetectionZone cliffDetectionZone;
 
     Rigidbody2D rb;
     TouchingDirections touchingDirections;
@@ -61,6 +62,14 @@ public class Knight : MonoBehaviour
         }
     }
 
+    public float AttackCooldown { get
+        {
+            return animator.GetFloat(AnimationStrings.attackCooldown);
+        } private set {
+            animator.SetFloat(AnimationStrings.attackCooldown, Mathf.Max(value, 0));
+        }
+    }
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -73,6 +82,11 @@ public class Knight : MonoBehaviour
     void Update()
     {
         HasTarget = attackZone.detectedColliders.Count > 0;
+
+        if (AttackCooldown > 0 )
+        {
+            AttackCooldown -= Time.deltaTime;
+        }
     }
 
     private void FixedUpdate()
@@ -112,6 +126,14 @@ public class Knight : MonoBehaviour
     public void OnHit(int damage, Vector2 knockback)
     {
         rb.velocity = new Vector2(knockback.x, rb.velocity.y + knockback.y);
+    }
+
+    public void OnCliffDetected()
+    {
+        if (touchingDirections.IsGrounded)
+        {
+            FlipDirection();
+        }
     }
 
 }
